31c992ada2eebf1be300f41ff54575fdbf673c03
Miljana0609 <miljanazoranovic25@gmail.com>
2026-01-19 20:27:21 +0100

    Allow Koyeb host

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    preview: {
+        host: true,
+        port: 8080,
+        allowedHosts: ['https://inquisitive-emmye-miljanaa-0c55bd32.koyeb.app/']
+    }
 })
