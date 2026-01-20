ff9cd2f0d4de01ad0b7f64c517cbc5863ffd1f43
Miljana0609 <miljanazoranovic25@gmail.com>
2026-01-20 13:17:04 +0100

    Fix wall x1

--- src/components/Wall.jsx
+++ src/components/Wall.jsx
@@ -148,7 +148,7 @@ const Wall = () => {
             {posts.length === 0 && <p>Inga inlägg hittades</p>}
 
             <ul className="post-list">
-                {posts.map((post) => (
+                {Array.isArray(posts) && posts.map((post) => (
                     <li key={post.id} className="post-card">
                         <p className="post-text">{post.text}</p>
                         <hr/>
