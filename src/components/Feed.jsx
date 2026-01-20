f0f7eb82f95df77c11a711c16259936750a6966e
Miljana0609 <miljanazoranovic25@gmail.com>
2026-01-20 13:21:47 +0100

    Fix wall x2

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -76,7 +76,7 @@ const Feed = () => {
             {posts.length === 0 && <p>Inga inlägg hittades</p>}
 
             <ul className="post-list">
-                {posts.map((post) => (
+                {posts.content.map((post) => (
                     <li key={post.id} className="post-card">
                         <p className="post-text">{post.text}</p>
                         <hr/>
